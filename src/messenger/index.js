8db59ff31bc86fea623a9ff52a77190b1fdd4505
Raphael Chookagian <raphaelchookagian@gmail.com>
2022-12-13 17:36:08 -0800

    adding UML and information to README

--- src/messenger/index.js
+++ src/messenger/index.js
@@ -6,4 +6,16 @@ const socket = io('http://localhost:3001');
 const Chance = require('chance');
 const chance = new Chance();
 
-socket.on('RECEIVED', )
+socket.on('RECEIVED', handleReceived);
+
+function handleReceived(payload) {
+  console.log(`Receipt confirmed to: ${payload.text.split(' ').pop()}`);
+
+}
+
+setInterval(() => {
+  const text = `Hi ${chance.first()}`;
+  console.log('Sending MESSAGE:', text);
+  // sendMessage(`Hi ${chance.first()}`);
+  socket.emit('MESSAGE', { text });
+}, 3000);
